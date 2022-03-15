07a4995d6614b17c2eda148ad24c9a6c2f490401
Akash Biswas <64808720+akshbswas98@users.noreply.github.com>
2022-03-15 12:36:00 +0530

    second commit

--- birthdayReminder.js
+++ birthdayReminder.js
@@ -1,18 +1,10 @@
-//// HEADING //////
 const today = new Date();
 const options = { weekday: "long",day: "numeric",month: "long",
 };
 
 document.getElementById("heading").innerHTML = today.toLocaleDateString("en-US",options);
-
-//// DATA /////
-
-// for making birth date to current date to show content
-// you can change birth dates to current dates to see person in results
 var d = new Date();
 var currentMonthAndDate =(d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1) + "/" + d.getDate();
-
-// Date :  MM/DD/YYYY
 const data = [
 {
     id: 1,
@@ -34,10 +26,7 @@ const data = [
 },
 
 ];
-
-
 var todayData = [];
-
 const currentDate = new Date().getDate();
 const currentMonth = new Date().getMonth();
 
@@ -47,14 +36,9 @@ return (
     parseInt(date.substring(0, 2)) === currentMonth + 1
 );
 });
-
-/// DISPLAY ////
-
 var noOfBirthdays = document.getElementById("noOfBirthdays");
 noOfBirthdays.innerHTML = todayData.length + " birthdays today";
-
 var people = document.getElementById("people");
-
 todayData.forEach((person) => {
 const { id, name, date, image } = person;
 
@@ -64,39 +48,28 @@ article.setAttribute("class", "person");
 var img = new Image();
 img.src = image;
 img.setAttribute("alt", name);
-
 var info = document.createElement("div");
-
 var h4 = document.createElement("h4");
 h4.setAttribute("id", "name");
 h4.innerHTML = name;
-
 var p = document.createElement("p");
 var dob = new Date(date);
   //calculate month difference from current date in time
 var month_diff = Date.now() - dob.getTime();
-
   //convert the calculated difference in date format
 var age_dt = new Date(month_diff);
-
   //extract year from date
 var year = age_dt.getUTCFullYear();
-
   //now calculate the age of the user
 var age = Math.abs(year - 1970);
 p.innerHTML = age + " years";
-
 info.appendChild(h4);
 info.appendChild(p);
-
 article.appendChild(img);
 article.appendChild(info);
-
 people.appendChild(article);
 });
-
 //// clear ////
-
 function clearAll() {
 people.innerHTML = "";
 noOfBirthdays.innerHTML = "There are no birthdays available today";
